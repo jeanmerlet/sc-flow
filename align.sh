940c5da39957a68e119294d4072be73e8cc3011d
Jean Merlet <jean.joseph.merlet@gmail.com>
2023-05-17 15:44:36 -0400

    update align.sh

--- align.sh
+++ align.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 fastqc_bin=$1
-data_dir=$2
-out_dir=$3
-job_script=scripts/jobs/fastqc.sbatch
+star_bin=$2
+genome_dir=$3
+bc_whitelist=$4
+umi_len=$5
+data_dir=./data/raw
+out_fastqc_dir=./qc/reads
+out_align_dir=./data/bam
+out_alignqc_dir=./qc/alignment
+
 
 export num_files=$(find $data_dir -name '*.fastq.gz' | tr ' ' '\n' | wc -l)
 # round correctly for truncated arithmetic
-export num_files=$((( $num_files + 1 ) / 2 ))
+export num_paired_files=$((( $num_files + 1 ) / 2 ))
 
+# fastqc
 printf \
 "#!/bin/bash
 
 #SBATCH -A syb111
-#SBATCH -N $num_files
+#SBATCH -N $num_paired_files
 #SBATCH -t 6:00:00
 #SBATCH -J fastqc
 #SBATCH -o ./scripts/alignment/logs/fastqc.%%J.out
@@ -22,10 +29,36 @@ printf \
 module load python
 
 echo fastqc_bin $fastqc_bin
-echo data_dir $data_dir
-echo out_dir $out_dir
 
-srun -n 2 python ./scripts/alignment/mpi_fastqc.py $fastqc_bin $data_dir $out_dir" \
-> $job_script
+srun -n 2 python ./scripts/alignment/mpi_fastqc.py $fastqc_bin $data_dir $out_fastqc_dir" \
+#rm $out_fastqc_dir/*.zip
+#multiqc $out_fastqc_dir
+#mv $out_fastqc_dir/multiqc_report.html $out_fastqc_dir/fastqc_report.html" \
+> $fastqc_job_script
 
-sbatch $job_script
+sbatch $fastqc_job_script
+
+
+# alignment and alignment qc
+printf \
+"#!/bin/bash
+
+#SBATCH -A SYB111
+#SBATCH -N $num_paired_files
+#SBATCH -t 6:00:00
+#SBATCH -J STAR_align
+#SBATCH -o ./logs/align.%%J.out
+#SBATCH -e ./logs/align.%%J.err
+
+module load python
+
+echo star_bin $star_bin
+
+srun -n 1 python ./scripts/alignment/mpi_align.py $star_bin $genome_dir $bc_whitelist $data_dir $out_align_dir $umi_len
+
+source load /gpfs/alpine/syb105/proj-shared/Personal/atown/Libraries/Andes/Anaconda3/envs/python_andes
+srun -N 1 -n 1 multiqc $out_align_dir
+mv $out_align_dir/multiqc_report.html $out_alignqc_dir/align_report.html" \
+> $align_job_script
+
+#sbatch $align_job_script
---
+++ scripts/alignment/mpi_align.py
@@ -0,0 +1,60 @@
+from mpi4py import MPI
+import subprocess
+import sys, os, re
+
+# script arguments
+star_bin = sys.argv[1]
+genome_dir = sys.argv[2]
+bc_whitelist = sys.argv[3]
+data_dir = sys.argv[4]
+out_dir = sys.argv[5]
+umi_len = sys.argv[6]
+
+
+# alignment function
+def sc_star_align(fastq1, fastq2, prefix, umi_len):
+    out_prefix = os.path.join(out_dir, prefix + '_')
+    subprocess.run([star,
+                    '--runThreadN', '16',
+                    '--genomeDir', genome,
+                    '--soloCBwhitelist', bc_whitelist,
+                    '--soloCBmatchWLtype', '1MM_multi_Nbase_pseudocounts',
+                    '--soloCBlen', '16',
+                    '--soloUMIstart', '17',
+                    '--soloUMIlen', umi_len,
+                    '--soloBarcodeReadLength', '0',
+                    '--soloCellFilter','EmptyDrops_CR',
+                    '--soloType CB_UMI_Simple',
+                    '--soloFeatures', 'Gene',
+                    '--soloUMIdedup', '1MM_CR',
+                    '--soloUMIfiltering', 'MultiGeneUMI_CR',
+                    '--clipAdapterType', 'CellRanger4',
+                    '--outFilterScoreMin', '30',
+                    '--readFilesIn', fastq2, fastq1,
+                    '--outFileNamePrefix', out_prefix,
+                    '--outSAMtype', 'BAM', 'SortedByCoordinate'])
+
+
+fastq_paths = os.listdir(data_dir)
+fastq_paths.sort()
+
+paired_fastq_list = []
+pair = []
+for i, fastq_path in enumerate(fastq_paths):
+    pair.append(os.path.join(data_dir, fastq_path))
+    if i % 2 == 1:
+        paired_fastq_list.append(pair)
+        pair = []
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+for i, pair in enumerate(paired_fastq_list):
+    if i % size != rank: continue
+    fastq_path1, fastq_path2 = pair
+    head, fastq = os.path.split(fastq_path1)
+    prefix = re.search('(.*)[rR][12].*', fastq).groups()[0]
+    if prefix[-1] == '_': prefix = prefix[:-1]
+    print(f'{prefix} (task number {i}) being aligned by {rank} of {size}')
+    sc_star_align(fastq_path1, fastq_path2, prefix)
