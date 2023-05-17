ba7b489f974196b52b0205f94c67b54d8be8cb34
Jean Merlet <jean.joseph.merlet@gmail.com>
2023-05-17 16:12:56 -0400

    finish alignment scripts

--- align.sh
+++ align.sh
@@ -5,10 +5,13 @@ star_bin=$2
 genome_dir=$3
 bc_whitelist=$4
 umi_len=$5
+
 data_dir=./data/raw
 out_fastqc_dir=./qc/reads
 out_align_dir=./data/bam
 out_alignqc_dir=./qc/alignment
+fastqc_job_script=./scripts/jobs/fastqc.sbatch
+align_job_script=./scripts/jobs/align.sbatch
 
 
 export num_files=$(find $data_dir -name '*.fastq.gz' | tr ' ' '\n' | wc -l)
@@ -30,10 +33,10 @@ module load python
 
 echo fastqc_bin $fastqc_bin
 
-srun -n 2 python ./scripts/alignment/mpi_fastqc.py $fastqc_bin $data_dir $out_fastqc_dir" \
-#rm $out_fastqc_dir/*.zip
-#multiqc $out_fastqc_dir
-#mv $out_fastqc_dir/multiqc_report.html $out_fastqc_dir/fastqc_report.html" \
+srun -n $num_files python ./scripts/alignment/mpi_fastqc.py $fastqc_bin $data_dir $out_fastqc_dir
+rm $out_fastqc_dir/*.zip
+multiqc $out_fastqc_dir
+mv $out_fastqc_dir/multiqc_report.html $out_fastqc_dir/fastqc_report.html" \
 > $fastqc_job_script
 
 sbatch $fastqc_job_script
@@ -47,18 +50,19 @@ printf \
 #SBATCH -N $num_paired_files
 #SBATCH -t 6:00:00
 #SBATCH -J STAR_align
-#SBATCH -o ./logs/align.%%J.out
-#SBATCH -e ./logs/align.%%J.err
+#SBATCH -o ./scripts/alignment/logs/align.%%J.out
+#SBATCH -e ./scripts/alignment/logs/align.%%J.err
 
 module load python
 
 echo star_bin $star_bin
 
-srun -n 1 python ./scripts/alignment/mpi_align.py $star_bin $genome_dir $bc_whitelist $data_dir $out_align_dir $umi_len
+srun -n $num_paired_files python ./scripts/alignment/mpi_align.py $star_bin $genome_dir $bc_whitelist $data_dir $out_align_dir $umi_len
 
 source load /gpfs/alpine/syb105/proj-shared/Personal/atown/Libraries/Andes/Anaconda3/envs/python_andes
 srun -N 1 -n 1 multiqc $out_align_dir
 mv $out_align_dir/multiqc_report.html $out_alignqc_dir/align_report.html" \
 > $align_job_script
 
-#sbatch $align_job_script
+sbatch $align_job_script
+
--- scripts/alignment/mpi_align.py
+++ scripts/alignment/mpi_align.py
@@ -14,9 +14,9 @@ umi_len = sys.argv[6]
 # alignment function
 def sc_star_align(fastq1, fastq2, prefix, umi_len):
     out_prefix = os.path.join(out_dir, prefix + '_')
-    subprocess.run([star,
+    subprocess.run([star_bin,
                     '--runThreadN', '16',
-                    '--genomeDir', genome,
+                    '--genomeDir', genome_dir,
                     '--soloCBwhitelist', bc_whitelist,
                     '--soloCBmatchWLtype', '1MM_multi_Nbase_pseudocounts',
                     '--soloCBlen', '16',
@@ -30,6 +30,7 @@ def sc_star_align(fastq1, fastq2, prefix, umi_len):
                     '--soloUMIfiltering', 'MultiGeneUMI_CR',
                     '--clipAdapterType', 'CellRanger4',
                     '--outFilterScoreMin', '30',
+                    '--readFilesCommand', 'zcat',
                     '--readFilesIn', fastq2, fastq1,
                     '--outFileNamePrefix', out_prefix,
                     '--outSAMtype', 'BAM', 'SortedByCoordinate'])
@@ -57,4 +58,4 @@ for i, pair in enumerate(paired_fastq_list):
     prefix = re.search('(.*)[rR][12].*', fastq).groups()[0]
     if prefix[-1] == '_': prefix = prefix[:-1]
     print(f'{prefix} (task number {i}) being aligned by {rank} of {size}')
-    sc_star_align(fastq_path1, fastq_path2, prefix)
+    sc_star_align(fastq_path1, fastq_path2, prefix, umi_len)
