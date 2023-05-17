47ece24dd0b2bc16776d835cf9550db9b0f86155
Jean Merlet <jean.joseph.merlet@gmail.com>
2023-05-17 11:03:33 -0400

    add directory generator

---
+++ project_init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# NOTE: run in project root
+
+## set up directory structure
+# data
+mkdir -p data
+cd data
+mkdir -p bam
+mkdir -p count_matrices
+mkdir -p de
+mkdir -p metadata
+mkdir -p raw
+mkdir -p seurat_objects
+# plots
+cd ..
+mkdir -p plots
+cd plots
+mkdir -p cells
+mkdir -p de
+mkdir -p genes
+mkdir -p qc
+# qc
+cd ..
+mkdir -p qc
+cd qc
+mkdir -p alignment
+mkdir -p reads
+# scripts
+cd ..
+mkdir -p scripts
+cd scripts
+mkdir -p alignment
+mkdir -p download
+mkdir -p plots
+mkdir -p preprocess
+mkdir -p seurat
