0b5e2c75ede6bc22bc4f8b1990f5a9b6c920923a
ayeTown <al1c3@outlook.com>
2024-04-09 22:28:13 -0400

    adjusting diff_exp.R file to include option for DE analysis by condition

--- scripts/seurat/diff_exp.R
+++ scripts/seurat/diff_exp.R
@@ -1,18 +1,33 @@
 suppressPackageStartupMessages({
     library(Seurat)
     library(dplyr)
+    library(future)
+    library(data.table)
+    library(doParallel)
 })
 
+options(future.globals.maxSize = 100000 * 1024^2,future.rng.onMisue = "ignore")
 
-run_diff_exp <- function(obj, de_dir, diff_type, p_value) {
+run_diff_exp <- function(obj, de_dir, diff_type, condition_group, p_value) {
     start_time <- Sys.time()
-    if (diff_type == 'cluster') {
-        Idents(obj) <- obj@meta.data$clusters
-    } else if (diff_type == 'placeholder') {
-    }
+    Idents(obj) <- obj@meta.data$clusters
     obj <- FindVariableFeatures(obj)
-    markers <- FindAllMarkers(obj, assay='RNA')
-    markers <- markers %>% filter(p_val_adj < p_value) %>% arrange(cluster, desc(avg_log2FC)) %>% data.frame()
+    if (diff_type == 'cluster') {
+        plan('multicore',workers = 8)
+        markers <- FindAllMarkers(obj, assay='RNA')
+    } else if (diff_type == 'condition') {
+        cl <- makeCluster(8)
+        registerDoParallel(cl)
+        markers <- foreach(clust = sort(unique(obj$clusters)),.packages = c("Seurat")) %dopar% {
+            test <- FindMarkers(obj,ident.1 = condition_group,group.by = "condition",subset.ident = clust)
+            test$gene <- rownames(test)
+            test$clusters <- clust
+            return(test)
+        }
+        stopCluster(cl)
+        markers <- rbindlist(markers)    
+    }
+    markers <- markers %>% filter(p_val_adj < p_value) %>% arrange(clusters, desc(avg_log2FC)) %>% data.frame()
     filename <- paste0('degs_diff-type-', diff_type, '_p-value-', p_value, '.tsv')
     out_path <- paste0(de_dir, filename)
     write.table(markers, out_path, sep='\t', row.names=TRUE, col.names=TRUE, quote=FALSE)
